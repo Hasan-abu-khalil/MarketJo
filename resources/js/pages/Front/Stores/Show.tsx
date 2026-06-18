44b4f0e53c6a6ba8d1da68c01cc0848bea64047e
Hasan Abu Khalil <hasan.aak1998@gmail.com>
2026-06-18 23:47:06 +0300

    Fix import

--- resources/js/pages/Front/Stores/Show.tsx
+++ resources/js/pages/Front/Stores/Show.tsx
@@ -1,14 +1,13 @@
 import ProductItemCard from '@/components/Front/Product/ProductItemCard';
 import Pagination from '@/components/Front/Shared/Pagination';
 import useAutoRefresh from '@/hooks/useAutoRefresh';
-import FrontLayout from '@/Layouts/FrontLayout';
+import FrontLayout from '@/layouts/FrontLayout';
 import { Link } from '@inertiajs/react';
 
 export default function Show({ store, products }) {
-        useAutoRefresh(['stores']);
-        useAutoRefresh(['products']);
+    useAutoRefresh(['stores']);
+    useAutoRefresh(['products']);
 
-    
     return (
         <FrontLayout>
             <div className="mx-auto max-w-7xl px-4 py-10">
